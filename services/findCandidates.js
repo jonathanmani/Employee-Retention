55ef13758caa6d58dfdcbf39e54c706827a847d6
John Malapit <johncmalapit@gmail.com>
2022-09-10 15:41:51 -0400

    create findCandidates service

---
+++ services/findCandidates.js
@@ -0,0 +1,7 @@
+const findCandidates = () => {
+  // check job title - any match or string comparison
+  // check for start and end dates - check if overlap
+  // threshold
+}
+
+export default findCandidates;
