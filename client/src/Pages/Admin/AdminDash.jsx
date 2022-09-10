a73b19346f3ba257748716fb3a24c108fab13224
Dominic Quintilian <dquintilian@gmail.com>
2022-09-10 16:45:36 -0400

    fixes

---
+++ client/src/Pages/Admin/AdminDash.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AppNav from '../../Layouts/AppNav'
+import Page from '../../Layouts/Page'
+
+const AdminDash = () => {
+  return (
+    <div>
+      <AppNav>
+        <Page>
+
+        </Page>
+      </AppNav>
+    </div>
+  );
+};
+
+export default AdminDash;
