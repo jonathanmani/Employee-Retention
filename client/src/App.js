77d1d58ad879d381d3aba7bcf379019f820dd37b
Dominic Quintilian <dquintilian@gmail.com>
2022-09-10 12:21:15 -0400

    test push

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
             exact
             path="/forgotpassword"
             element={<ForgotPasswordPafge />}
-          />
+          /> 
           <Route
             exact
             path="/passwordreset/:resetToken"
