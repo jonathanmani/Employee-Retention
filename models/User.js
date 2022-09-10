93b03bc4b9325540e90b3ee8311c9df2b6b9240f
Dominic Quintilian <dquintilian@gmail.com>
2022-09-10 15:00:19 -0400

    authentication works now!

--- models/User.js
+++ models/User.js
@@ -33,11 +33,11 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    required: [true, 'Please select a role'],
+    // required: [true, 'Please select a role'],
   },
   candidateType: {
     type: String,
-    required: [true, 'Please select a role'],
+    // required: [true, 'Please select a role'],
   },
   company: {
     type: mongoose.Schema.Types.ObjectId,
