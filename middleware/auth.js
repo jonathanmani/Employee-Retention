44e538e05eaa3f76120082e6deddd708f94b8ec4
Dominic Quintilian <dquintilian@gmail.com>
2022-09-10 12:33:26 -0400

    test

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ exports.protect = async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
-  if (!token) {
+  if (!token) { 
 
     return next(new ErrorResponse("Not authorized to access this route", 401));
   }
