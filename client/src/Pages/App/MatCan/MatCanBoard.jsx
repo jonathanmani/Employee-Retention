aab1e73a294e193d57e1a0775bd4c6a2ceb55c81
Jonathan Mani <jonathanmani@protonmail.com>
2022-09-10 20:50:31 -0400

    started onboarding

--- client/src/Pages/App/MatCan/MatCanBoard.jsx
+++ client/src/Pages/App/MatCan/MatCanBoard.jsx
@@ -6,7 +6,7 @@ const MatCanBoard = () => {
     <div>
         <h1 class='text-center fs-2'>You are eligible for:</h1>
         < Representative />
-        <button type="button" class="btn btn-outline-success btn-sm">Success</button>
+        <button type="submit" class="btn btn-outline-success btn-sm">Send</button>
     </div>
   )
 }
---
+++ client/src/Pages/App/MatRep/MatRepOnboarding.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MatRepOnboarding = ({ companyName, jobTitle, Skills }) => {
+  return (
+    <div>
+        <h1 class='text-center fs-2'>Fill Your Profile</h1>
+        <button type="submit" class="btn btn-outline-success btn-sm">Create profile</button>
+    </div>
+  )
+}
+
+export default MatRepOnboarding
